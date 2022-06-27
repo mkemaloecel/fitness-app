2d01ac703b419f7c0c8f2fcd666697be9335cd15
mustafa.kemal.oecel <mustafa.kemal.oecel@gmail.com>
2022-06-27 13:37:00 +0200

    config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { Store } from 'store';
+import { Store } from '../store';
 
 // feature modules
 
